4855c74fbf694fb387b89b66bd985e0038809786
Lenz Weber <mail@lenzw.de>
2019-01-26 00:50:12 +0100

    start

---
+++ src/game/level.tsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Sprite, TileMap } from "react-game-kit";
+export default class Level extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          transform: `translate(${0}px, 0px) translateZ(0)`,
+          transformOrigin: "top left"
+        }}
+      >
+        <TileMap
+          //columns={37}
+          //rows={28}
+          //width={628}
+          //height={475}
+          columns={8}
+          rows={2}
+          tileSize={16}
+          src={require("../assets/roguelikeCity_magenta.png")}
+          layers={[[1, 1, 1, 3, 2, 4, 2, 1], [4, 2, 2, 1, 1, 4, 2, 5]]}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/store.ts
@@ -0,0 +1,24 @@
+import {
+  createSlice,
+  configureStore,
+  combineReducers
+} from "redux-starter-kit";
+
+export const playerStats = createSlice({
+  slice: "playerStats",
+  initialState: {
+    energy: 100
+  },
+  reducers: {
+    setEnergy: (state, { payload: energy }) => ({ energy })
+  }
+});
+
+export const store = configureStore({
+  reducer: combineReducers({
+    playerStats: playerStats.reducer
+  }),
+  middleware: [],
+  devTools: true,
+  enhancers: []
+});
