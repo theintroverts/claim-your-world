afd3e96e81838ac33d0c9fce5cd4d0845a7c3c58
Stefan Siegl <stesie@brokenpipe.de>
2019-01-26 23:28:15 +0100

    track energy sources w/ redux

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,8 @@
+import { Omit } from 'react-redux';
 import { combineReducers, configureStore, createSlice, PayloadAction } from 'redux-starter-kit';
 import { EnhancedStore } from 'redux-starter-kit/src/configureStore';
+
+import { EnergySourceData } from './game/energySources';
 
 export const playerStats = createSlice({
     slice: 'playerStats',
@@ -22,10 +25,22 @@ export const playerLocation = createSlice({
     },
 });
 
+export const energySources = createSlice({
+    slice: 'energySources',
+    initialState: [] as Array<EnergySourceData>,
+    reducers: {
+        addEnergySource: (state, { payload: energySource }: PayloadAction<Omit<EnergySourceData, 'createdAt'>>) => [
+            ...state,
+            { ...energySource, createdAt: new Date() },
+        ],
+    },
+});
+
 export const store = configureStore({
     reducer: combineReducers({
         playerStats: playerStats.reducer,
         playerLocation: playerLocation.reducer,
+        energySources: energySources.reducer,
     }),
     middleware: [],
     devTools: true,
