ba9b759441cdc67ef4e4ae1ae0c685001a173c44
Lenz Weber <mail@lenzw.de>
2019-01-26 14:20:58 +0100

    optimize collsision layer generation

--- src/util/layer.ts
+++ src/util/layer.ts
@@ -1,4 +1,5 @@
-import Matter, { Bodies, Composite } from 'matter-js';
+import { rejects } from 'assert';
+import Matter, { Bodies, Body, Composite } from 'matter-js';
 
 export interface Layer {
     data: number[];
@@ -52,20 +53,70 @@ export type TileData = {
     tsxJs: TsxJson;
 };
 
+interface Rect {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
 export function layerToComposite(layer: Layer): Composite {
     const composite = Composite.create();
 
-    for (let dx = 0; dx < layer.width; dx++) {
-        for (let dy = 0; dy < layer.height; dy++) {
+    const rectMap: Array<Array<Rect>> = [];
+    const getRect = (x: number, y: number): Rect | undefined => (rectMap[y] || [])[x];
+    const setRect = (x: number, y: number, rect: Rect) => {
+        if (!rectMap[y]) {
+            rectMap[y] = [];
+        }
+        rectMap[y][x] = rect;
+    };
+
+    const buildRect = (x: number, y: number, width: number, height: number, options = { isStatic: true }) =>
+        Bodies.rectangle(x + width / 2, y + height / 2, width, height, options);
+
+    for (let dy = 0; dy < layer.height; dy++) {
+        for (let dx = 0; dx < layer.width; dx++) {
             const x = layer.x + dx;
             const y = layer.y + dy;
-            const value = layer.data[y * layer.width + x];
+            const value = layer.data[dy * layer.width + dx];
 
             if (value) {
-                Composite.add(composite, Bodies.rectangle(x, y, 1, 1, { isStatic: true }));
+                let rect = { x, y, width: 1, height: 1 };
+                const left = getRect(x - 1, y);
+                if (left && left.height === 1) {
+                    // grow right
+                    left.width++;
+                    rect = left;
+                }
+
+                const top = getRect(x, y - 1);
+                if (top && top.x == rect.x && top.width === rect.width) {
+                    // grow bottom
+                    for (let x1 = rect.x; x1 < rect.x + rect.width; x1++) {
+                        setRect(x1, y, top);
+                    }
+                    top.height++;
+                    rect = top;
+                }
+
+                setRect(x, y, rect);
             }
         }
     }
+
+    const acc: Rect[] = [];
+    for (const row of rectMap) {
+        if (!row) {
+            continue;
+        }
+        for (const rect of row) {
+            if (rect && !acc.includes(rect)) {
+                acc.push(rect);
+            }
+        }
+    }
+    acc.forEach(rect => Composite.add(composite, buildRect(rect.x, rect.y, rect.width, rect.height)));
 
     return composite;
 }
@@ -78,6 +129,5 @@ export function extractTmxCollisionComposite(tmx: TmxJson): Composite {
             Composite.add(composite, layerToComposite(layer));
         }
     }
-
     return composite;
 }
