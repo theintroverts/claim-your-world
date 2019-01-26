fd6df70a2f55084408738f999d0f10ebb253a9c6
Stefan Siegl <stesie@brokenpipe.de>
2019-01-26 19:04:44 +0100

    stop the game if energy drops to zero

--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { KeyListener, Loop, Stage } from 'react-game-kit';
+import { connect } from 'react-redux';
 
+import { State } from '../store';
 import { TmxJson, TsxJson } from '../util/layer';
 import GameStats from './GameStats';
 import IntroWorld from './IntroWorld';
@@ -10,7 +12,11 @@ import Viewport from './Viewport';
 const tmxJs: TmxJson = require('../assets/ClaimYourWorld.tmx.json');
 const tsxJs: TsxJson = require('../assets/ClaimYourWorld.tsx.json');
 
-export default class Game extends Component {
+export interface Props {
+    energy: number;
+}
+
+class Game extends Component<Props> {
     componentDidMount() {
         this.keyListener.subscribe([
             this.keyListener.LEFT,
@@ -25,7 +31,13 @@ export default class Game extends Component {
         this.keyListener.unsubscribe();
     }
 
+    isAlive = () => this.props.energy > 0;
+
     render() {
+        if (!this.isAlive()) {
+            return <div style={{ background: 'red' }}>You lost the game.</div>;
+        }
+
         return (
             <Loop>
                 <Stage width={800} height={450} style={this.getStageStyles()}>
@@ -62,3 +74,5 @@ export default class Game extends Component {
 
     private keyListener = new KeyListener();
 }
+
+export default connect(({ playerStats: { energy } }: State) => ({ energy }))(Game);
